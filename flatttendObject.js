b30aa3e6ecec695b8515ead2708f10801ad2f3c5
sumitra1 <sumitrakumarishaw1@gmail.com>
2023-05-07 19:11:53 +0530

    assignment on removing duplicate id

---
+++ flatttendObject.js
@@ -0,0 +1,38 @@
+const input = {
+    A: "12",
+    B: 23,
+    C: {
+      P: 23,
+      O: {
+        L: 56,
+      },
+      Q: [1, 2],
+    },
+  };
+  
+  
+  // { A: '12', B: 23, 'C.P': 23, 'C.O.L': 56, 'C.Q.0': 1, 'C.Q.1': 2 }
+  
+  const flattenObject=(inputObj,prefix)=>{
+      console.log(prefix);
+      let outputObj={};
+      for(const [key,value] of Object.entries(inputObj)){
+          const newKey=prefix? prefix+'.'+key: key;
+          // console.log(key,value);
+         if(typeof value!=="object"){
+          outputObj[newKey]= value;
+         }else{
+          const outputObj2= flattenObject(value,newKey);  
+          outputObj={...outputObj,...outputObj2};
+         }
+      }
+      return outputObj;  
+  }
+  
+  console.log(flattenObject(input));
+  
+  
+  
+  
+  
+  
---
+++ removeDuplicateKeyFromObject.js
@@ -0,0 +1,41 @@
+const data = {
+    123: {
+      name: "Alice",
+      id: 123,
+      dob: "Dec"
+    },
+    456: {
+      name: "Bob",
+      id: 4567,
+      dob: "Nov"
+    }
+  };
+  
+  const data2 = {
+    456: {
+      name: "Bob",
+      id: 456,
+      dob: "Nov",
+      eth: "hi"
+    },
+    135: {
+      name: "Eve",
+      id: 135,
+      dob: "Jun"
+    }
+  };
+  
+  const output = {};
+ 
+// Combine data and data2 into the output object
+
+for(const [key,value] of Object.entries(data)){
+    output[key]={...value,id:key};
+}
+for(const [key,value] of Object.entries(data2)){
+    output[key]={...value,id:key};
+}
+
+
+console.log(output);
+  
